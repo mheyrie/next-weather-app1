3ebe1267c25e91dee28b5ef8a28424556567ca37
Mheyrie <omota4fidelity@gmail.com>
2024-06-09 10:17:52 +0100

    Implementing api routing

--- interfaces/city.ts
+++ interfaces/city.ts
@@ -1,3 +1,12 @@
 interface CityData {
     id: number;
+    name: string;
+    state: string;
+    country: string;
+    coord: {
+        lon: number;
+        lat: number;
+    };
 }
+
+export default CityData
